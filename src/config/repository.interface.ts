44d3f0f10768a78bf81d75fd402cccb3a0a6ac81
fermorello <ferminmorello@gmail.com>
2025-01-21 17:33:33 -0300

    Refactor IRepository interface to simplify find method signature

--- src/config/repository.interface.ts
+++ src/config/repository.interface.ts
@@ -2,11 +2,7 @@ import { BaseEntity } from "./base.entity";
 
 export default interface IRepository<T extends BaseEntity> {
   find(query?: { [key: string]: unknown }):
-    | Promise<T[] | null>
-    | Promise<{
-        data: T[] | null;
-        pagination: { total: number; pages: number };
-      }>;
+    | Promise<T[] | null>;
   findOne(id: T['id']): Promise<T | null>;
   create(entity: T | Partial<T>): Promise<T | null>;
   update(id: T['id'], entity: T | Partial<T>): Promise<T | null>;
