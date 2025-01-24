45d0140dedcebc7d64a3536746e81b026e746a5e
fermorello <ferminmorello@gmail.com>
2025-01-24 12:47:03 -0300

    Add PortfolioServiceImp to UserController in user.routes

--- src/marketdata/router/user.routes.ts
+++ src/marketdata/router/user.routes.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import { UserServiceImp } from '../../shared/instances';
+import { PortfolioServiceImp, UserServiceImp } from '../../shared/instances';
 import { BaseRouter } from '../../config/base.router';
 import { UserController } from '../../users/controller/user.controller';
 
-const userController = new UserController(UserServiceImp);
+const userController = new UserController(UserServiceImp, PortfolioServiceImp);
 
 export default class InstrumentRouter extends BaseRouter<UserController> {
   constructor() {
