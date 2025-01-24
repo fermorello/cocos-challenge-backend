51395289b4c30f12a73ad7034ce16687f3e4a8bd
fermorello <ferminmorello@gmail.com>
2025-01-24 12:59:50 -0300

    Refactor createOrderSchema to include OrderSide and OrderType enums, and add validation for size or amount and price for LIMIT orders

--- src/orders/dto/createOrder.dto.ts
+++ src/orders/dto/createOrder.dto.ts
@@ -1,38 +1,66 @@
 import { z, ZodTypeAny } from 'zod';
-import { OrderSide, OrderType } from '../interfaces/order.interface';
 
-export const createOrderSchema: any = z.object({
-  userId: z
-    .number({
-      required_error: 'userId is required',
-      invalid_type_error: 'userId must be a number',
-    })
-    .int()
-    .positive(),
-  instrumentId: z
-    .number({
-      required_error: 'instrumentId is required',
-      invalid_type_error: 'instrumentId must be a number',
-    })
-    .int()
-    .positive(),
+export const OrderSide = {
+  BUY: 'BUY',
+  SELL: 'SELL',
+  CASH_IN: 'CASH_IN',
+  CASH_OUT: 'CASH_OUT',
+} as const;
 
-  side: z.enum(
-    [OrderSide.BUY, OrderSide.SELL, OrderSide.CASH_IN, OrderSide.CASH_OUT],
+export const OrderType = {
+  MARKET: 'MARKET',
+  LIMIT: 'LIMIT',
+} as const;
+
+export const createOrderSchema: any = z
+  .object({
+    userId: z
+      .number({
+        required_error: 'userId is required',
+        invalid_type_error: 'userId must be a number',
+      })
+      .int()
+      .positive(),
+    instrumentId: z
+      .number({
+        required_error: 'instrumentId is required',
+        invalid_type_error: 'instrumentId must be a number',
+      })
+      .int()
+      .positive(),
+
+    side: z.enum(
+      [OrderSide.BUY, OrderSide.SELL, OrderSide.CASH_IN, OrderSide.CASH_OUT],
+      {
+        required_error: 'side is required',
+        invalid_type_error: 'Invalid order side',
+      }
+    ),
+
+    type: z.enum([OrderType.MARKET, OrderType.LIMIT], {
+      required_error: 'type is required',
+      invalid_type_error: 'Invalid order type',
+    }),
+
+    size: z.number().positive().optional(),
+    amount: z.number().positive().optional(),
+    price: z.number().positive().optional(),
+  })
+  .refine((data) => data.size || data.amount, {
+    message: 'Either size or amount must be provided',
+    path: ['size'],
+  })
+  .refine(
+    (data) => {
+      if (data.type === OrderType.LIMIT) {
+        return data.price !== undefined;
+      }
+      return true;
+    },
     {
-      required_error: 'side is required',
-      invalid_type_error: 'Invalid order side',
+      message: 'Price is required for LIMIT orders',
+      path: ['price'],
     }
-  ),
-
-  type: z.enum([OrderType.MARKET, OrderType.LIMIT], {
-    required_error: 'type is required',
-    invalid_type_error: 'Invalid order type',
-  }),
-
-  size: z.number().positive().optional(),
-  amount: z.number().positive().optional(),
-  price: z.number().positive().optional(),
-});
+  );
 
 export type CreateOrderDTO = z.infer<typeof createOrderSchema>;
